1d0073ac040eec4c6cb542a3bee0e6ba075c16c3
Gymbotan <55342311+Gymbotan@users.noreply.github.com>
2023-02-14 14:45:03 +0300

    Feat: Update link to TuviKeyDerivationLib. (#28)
    * Feat: Update link to TuviKeyDerivationLib. Add PgpKeyBundle (deleted from TuviKeyDerivationLib). Update License text (2022 to 2023)
    
    * Ref: PgpKeyBundle as IEquatable

---
+++ TuviPgpLib/Entities/PgpKeyBundle.cs
@@ -0,0 +1,60 @@
+﻿///////////////////////////////////////////////////////////////////////////////
+//   Copyright 2023 Eppie (https://eppie.io)
+//
+//   Licensed under the Apache License, Version 2.0(the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+///////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Linq;
+
+namespace TuviPgpLib.Entities
+{
+    public class PgpPublicKeyBundle : PgpKeyBundle
+    {
+    }
+
+    public class PgpSecretKeyBundle : PgpKeyBundle
+    {
+    }
+
+    public class PgpKeyBundle : IEquatable<PgpKeyBundle>
+    {
+#pragma warning disable CA1819 // Properties should not return arrays
+        public byte[] Data { get; set; }
+#pragma warning restore CA1819 // Properties should not return arrays
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as PgpKeyBundle);
+        }
+
+        public bool Equals(PgpKeyBundle other)
+        {
+            if (this == other)
+            {
+                return true;
+            }
+            if (other == null)
+            {
+                return false;
+            }
+
+            return Data.SequenceEqual(other.Data);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
--- TuviPgpLib/IKeyStorage.cs
+++ TuviPgpLib/IKeyStorage.cs
@@ -1,5 +1,5 @@
 ﻿///////////////////////////////////////////////////////////////////////////////
-//   Copyright 2022 Eppie (https://eppie.io)
+//   Copyright 2023 Eppie (https://eppie.io)
 //
 //   Licensed under the Apache License, Version 2.0(the "License");
 //   you may not use this file except in compliance with the License.
@@ -17,6 +17,7 @@
 using KeyDerivation.Keys;
 using System.Threading;
 using System.Threading.Tasks;
+using TuviPgpLib.Entities;
 
 namespace TuviPgpLib
 {
